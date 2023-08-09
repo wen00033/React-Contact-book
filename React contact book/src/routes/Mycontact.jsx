7a163d6ad0040ff20b797ce7939d9eb2973673e9
Chi-hung Wen <wen00033@algonquinlive.com>
2023-08-09 01:05:48 -0400

    updated

--- React contact book/src/routes/Mycontact.jsx
+++ React contact book/src/routes/Mycontact.jsx
@@ -28,7 +28,9 @@ function myContact() {
         return data;
       }
       return data.filter((person) =>
-        person.firstName.toLowerCase().includes(search)
+        (
+          person.firstName.toLowerCase() + person.lastName.toLowerCase()
+        ).includes(search.toLowerCase())
       );
     };
     const filterItems = searchItems(search, data);
@@ -43,8 +45,7 @@ function myContact() {
           <input
             onChange={(e) => setSearch(e.target.value)}
             type="text"
-            name="first_name"
-            id="first_name"
+            placeholder="Search..."
             className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
           />
         </div>
